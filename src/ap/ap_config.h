e28100e80826adb31597f32931727707af60adcb
Michael Braun <michael-dev@fami-braun.de>
2015-04-21 12:18:34 +0200

    nl80211: fix CMD_FRAME used with VLANs
    This is required for 802.11r with VLANs used in combination with over_ds,
    or the action reply frame won't get out on WiFi.
    
    This is because kernel mac80211 CMD_FRAME uses sta_info_get instead of
    sta_info_get_bss, so the ifidx given needs to be the of the AP_VLAN iface.
    
    This could or should be fixed in kernel as well, but in order to be able to
    run the hwsim tests right now without rebuilding the kernel or in order to
    use FT with VLAN on older kernel, this change is needed.
    
    I'm happy to drop this and only use this as a local workaround until the
    kernel change is done.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/ap_config.h
+++ src/ap/ap_config.h
@@ -783,6 +783,8 @@ struct hostapd_config {
 #endif /* CONFIG_IEEE80211AX */
 };
 
+struct hostapd_data;
+
 
 int hostapd_mac_comp(const void *a, const void *b);
 struct hostapd_config * hostapd_config_defaults(void);
@@ -803,6 +805,8 @@ int hostapd_vlan_valid(struct hostapd_vlan *vlan,
 		       struct vlan_description *vlan_desc);
 const char * hostapd_get_vlan_id_ifname(struct hostapd_vlan *vlan,
 					int vlan_id);
+int hostapd_get_vlan_id_ifidx(struct hostapd_vlan *vlan, int vlan_id);
+int hostapd_get_sta_ifidx(struct hostapd_data *hapd, const u8 *addr);
 struct hostapd_radius_attr *
 hostapd_config_get_radius_attr(struct hostapd_radius_attr *attr, u8 type);
 int hostapd_config_check(struct hostapd_config *conf, int full_config);
