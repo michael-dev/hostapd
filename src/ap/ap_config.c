6bd2bc8edccd50efad7c942308a66bc9937aac01
Michael Braun <michael-dev@fami-braun.de>
2015-04-23 17:12:40 +0200

    FT: cache newly detected R0KH / R1KH when using wildcard
    This implements temporary positive caching for R0KH and R1KH seen when
    using wilcard r0kh/r1kh configuration. Cache entries expire after 1d.
    
    In order to free newly created stations later, the r*kh_list start pointer
    in conf needs to be updateable from wpa_auto_ft.c, where only wconf is
    accessed.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/ap_config.c
+++ src/ap/ap_config.c
@@ -90,6 +90,7 @@ void hostapd_config_defaults_bss(struct hostapd_bss_config *bss)
 
 #ifdef CONFIG_IEEE80211R
 	bss->ft_over_ds = 1;
+	bss->rkh_pos_timeout = 86400;
 #endif /* CONFIG_IEEE80211R */
 
 	bss->radius_das_time_window = 300;
