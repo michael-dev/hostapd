0d5aa7b29ce119c96077fb9270736d870240e906
Michael Braun <michael-dev@fami-braun.de>
2015-05-20 08:02:45 +0200

    vlan: move preauth snooping de/init into vlan_del/newlink
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/vlan_init.c
+++ src/ap/vlan_init.c
@@ -466,11 +466,6 @@ static int vlan_if_add(struct hostapd_data *hapd, struct hostapd_vlan *vlan,
 
 	ifconfig_up(vlan->ifname); /* else wpa group will fail fatal */
 
-#ifdef CONFIG_RSN_PREAUTH_COPY
-	if (!vlan->rsn_preauth)
-		vlan->rsn_preauth = rsn_preauth_snoop_init(hapd, vlan->ifname);
-#endif /* CONFIG_RSN_PREAUTH_COPY */
-
  	if (hapd->wpa_auth)
 		ret = hostapd_setup_wpa_vlan(hapd, vlan->vlan_id);
 
@@ -481,10 +476,6 @@ static int vlan_if_add(struct hostapd_data *hapd, struct hostapd_vlan *vlan,
 		   vlan->vlan_id, ret);
 	if (hostapd_desetup_wpa_vlan(hapd, vlan->vlan_id))
 		wpa_printf(MSG_ERROR, "WPA deinit of %s failed", vlan->ifname);
-
-#ifdef CONFIG_RSN_PREAUTH_COPY
-	rsn_preauth_snoop_deinit(hapd, vlan->ifname, vlan->rsn_preauth);
-#endif /* CONFIG_RSN_PREAUTH_COPY */
 
 	/* group state machine setup failed */
 	if (hostapd_vlan_if_remove(hapd, vlan->ifname))
@@ -501,11 +492,6 @@ static int vlan_if_remove(struct hostapd_data *hapd, struct hostapd_vlan *vlan)
 	if (ret)
 		wpa_printf(MSG_ERROR, "WPA deinitialization for vlan %d failed"
 			   " (%d)", vlan->vlan_id, ret);
-
-#ifdef CONFIG_RSN_PREAUTH_COPY
-	rsn_preauth_snoop_deinit(hapd, vlan->ifname, vlan->rsn_preauth);
-	vlan->rsn_preauth = NULL;
-#endif /* CONFIG_RSN_PREAUTH_COPY */
 
 	return hostapd_vlan_if_remove(hapd, vlan->ifname);
 }
@@ -570,6 +556,11 @@ static void vlan_newlink(char *ifname, struct hostapd_data *hapd)
 		}
 
 		ifconfig_up(ifname);
+
+#ifdef CONFIG_RSN_PREAUTH_COPY
+		if (!vlan->rsn_preauth)
+			vlan->rsn_preauth = rsn_preauth_snoop_init(hapd, vlan->ifname);
+#endif /* CONFIG_RSN_PREAUTH_COPY */
 
 		break;
 	}
@@ -652,6 +643,11 @@ static void vlan_dellink(char *ifname, struct hostapd_data *hapd)
 
 		if (!vlan->configured)
 			goto skip_counting;
+
+#ifdef CONFIG_RSN_PREAUTH_COPY
+		rsn_preauth_snoop_deinit(hapd, vlan->ifname,
+					 vlan->rsn_preauth);
+#endif /* CONFIG_RSN_PREAUTH_COPY */
 
 		notempty = vlan->vlan_desc.notempty;
 		untagged = vlan->vlan_desc.untagged;
