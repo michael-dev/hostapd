ed24d1fe6854ecfe5813b9fa8669bb6617ea8d31
Michael Braun <michael-dev@fami-braun.de>
2016-09-25 10:20:00 +0200

    FT: check mobility domain with RRB
    A malcious client might try to roam between different Mobility Domains, if the
    APs share their encryption key and reach each other using ft_iface.
    
    This change makes hostapd check the mobility domain with push and pull reply
    messages.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/wpa_auth.h
+++ src/ap/wpa_auth.h
@@ -82,6 +82,7 @@ struct ft_rrb_frame {
 #define FT_RRB_IDENTITY      15
 #define FT_RRB_RADIUS_CUI    16
 #define FT_RRB_SESSION_TIMEOUT  17 /* le32 seconds */
+#define FT_RRB_MOBILITY_DOMAIN  18 /* MOBILITY_DOMAIN_ID_LEN */
 
 struct ft_rrb_tlv {
 	le16 type;
@@ -97,7 +98,8 @@ struct ft_rrb_seq {
 
 /* session TLVs:
  *   required: PMK_R1, PMK_R1_NAME, PAIRWISE
- *   optional: VLAN, EXPIRES_IN, IDENTITY, RADIUS_CUI, SESSION_TIMEOUT
+ *   optional: VLAN, EXPIRES_IN, IDENTITY, RADIUS_CUI, SESSION_TIMEOUT,
+ *             MOBILITY_DOMAIN
  *
  * pull frame TLVs:
  *   auth:
