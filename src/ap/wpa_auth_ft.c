db2af3591a00044186fb01a1aa432010381abf97
Michael Braun <michael-dev@fami-braun.de>
2016-09-25 10:20:00 +0200

    FT: check mobility domain with RRB
    A malcious client might try to roam between different Mobility Domains, if the
    APs share their encryption key and reach each other using ft_iface.
    
    This change makes hostapd check the mobility domain with push and pull reply
    messages.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/wpa_auth_ft.c
+++ src/ap/wpa_auth_ft.c
@@ -2931,6 +2931,7 @@ static int wpa_ft_rrb_rx_pull(struct wpa_authenticator *wpa_auth,
 	struct ft_rrb_seq f_seq;
 	struct tlv_list rkh_id_local = { 0 };
 	struct tlv_list rkh_id_remote = { 0 };
+	const u8 *mdid = wpa_auth->conf.mobility_domain;
 
 	wpa_printf(MSG_DEBUG, "FT: Received PMK-R1 pull");
 
@@ -3030,6 +3031,9 @@ static int wpa_ft_rrb_rx_pull(struct wpa_authenticator *wpa_auth,
 		  .data = (u8 *) &f_seq },
 		rkh_id_local,
 		rkh_id_remote,
+		{ .type = FT_RRB_MOBILITY_DOMAIN,
+		  .len = MOBILITY_DOMAIN_ID_LEN,
+		  .data = mdid },
 		{ .type = FT_RRB_LAST_EMPTY, .len = 0, .data = NULL },
 	};
 
@@ -3079,10 +3083,12 @@ static int wpa_ft_rrb_rx_r1(struct wpa_authenticator *wpa_auth,
 	size_t f_r1kh_id_len, f_s1kh_id_len, f_r0kh_id_len;
 	const u8 *f_identity, *f_radius_cui;
 	const u8 *f_session_timeout;
+	const u8 *f_mobility_domain;
 	size_t f_pmk_r1_name_len, f_pairwise_len, f_pmk_r1_len;
 	size_t f_expires_in_len;
 	size_t f_identity_len, f_radius_cui_len;
 	size_t f_session_timeout_len;
+	size_t f_mobility_domain_len;
 	int pairwise;
 	int ret = -1;
 	struct tlv_list rkh_id_local = { 0 };
@@ -3092,6 +3098,7 @@ static int wpa_ft_rrb_rx_r1(struct wpa_authenticator *wpa_auth,
 	int session_timeout;
 	struct vlan_description vlan;
 	char buf[256];
+	const u8 *mdid = wpa_auth->conf.mobility_domain;
 
 	RRB_GET_AUTH(FT_RRB_R1KH_ID, r1kh_id, msgtype, FT_R1KH_ID_LEN);
 	if (os_memcmp_const(f_r1kh_id, wpa_auth->conf.r1_key_holder,
@@ -3179,6 +3186,21 @@ static int wpa_ft_rrb_rx_r1(struct wpa_authenticator *wpa_auth,
 	wpa_hexdump_key(MSG_DEBUG, "FT: PMK-R1", f_pmk_r1, PMK_LEN);
 	wpa_hexdump(MSG_DEBUG, "FT: PMKR1Name",
 		    f_pmk_r1_name, WPA_PMK_NAME_LEN);
+
+	RRB_GET_OPTIONAL(FT_RRB_MOBILITY_DOMAIN, mobility_domain, msgtype,
+			 MOBILITY_DOMAIN_ID_LEN);
+	if (f_mobility_domain) {
+		os_snprintf(buf, sizeof(buf), "FT: PMK-R1 %s - Mobility Domain",
+			    msgtype);
+		wpa_hexdump(MSG_DEBUG, buf, f_mobility_domain,
+			    f_mobility_domain_len);
+	}
+	if (f_mobility_domain &&
+	    os_memcmp(mdid, f_mobility_domain, MOBILITY_DOMAIN_ID_LEN) != 0) {
+		wpa_printf(MSG_DEBUG, "FT: PMK-R1 %s did not use a matching "
+			   "Mobility Domain", msgtype);
+		goto out;
+	}
 
 	pairwise = WPA_GET_LE16(f_pairwise);
 
@@ -3882,6 +3904,7 @@ static int wpa_ft_generate_pmk_r1(struct wpa_authenticator *wpa_auth,
 	size_t packet_len;
 	u8 f_timestamp[sizeof(le32)];
 	struct ft_rrb_seq f_seq;
+	const u8 *mdid = wpa_auth->conf.mobility_domain;
 
 	os_get_time(&now);
 	WPA_PUT_LE32(f_timestamp, now.sec);
@@ -3895,6 +3918,9 @@ static int wpa_ft_generate_pmk_r1(struct wpa_authenticator *wpa_auth,
 		  .data = s1kh_id },
 		{ .type = FT_RRB_PMK_R0_NAME, .len = WPA_PMK_NAME_LEN,
 		  .data = pmk_r0->pmk_r0_name },
+		{ .type = FT_RRB_MOBILITY_DOMAIN,
+		  .len = MOBILITY_DOMAIN_ID_LEN,
+		  .data = mdid },
 		{ .type = FT_RRB_LAST_EMPTY, .len = 0, .data = NULL },
 	};
 	struct tlv_list push_auth[] = {
