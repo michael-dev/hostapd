73f0db0f58df1ad7deb3033733d3928ee0213acb
Michael Braun <michael-dev@fami-braun.de>
2016-09-26 20:52:44 +0200

    macvlan: ft_iface and rsn_preauth_interfaces can be shared
    macvlan does not permit multiple BRIDGE type interfaces on the same lowerdev to
    share their mac address.
    
    So in order to share the lowerdev of the macvlan interfaces required by
    ft_iface and rsn_preauth_interfaces, they need to share their macvlan interface
    as well.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/macvlan.h
+++ src/ap/macvlan.h
@@ -2,8 +2,20 @@
 #define HOSTAPD_MACVLAN_H
 
 #ifdef CONFIG_LIBNL3_ROUTE
-int macvlan_add(const char *if_name, const u8 *addr, const char *if_base);
-int macvlan_del(const char *if_name);
+
+/* create or reuse macvlan interface if_name
+ * @param if_name  macvlan interface name, may be altered
+ * @param len      size of if_name buffer
+ * @param addr     hw ether address
+ * @param if_base  macvlan lowerdev
+ *
+ * @returns negative error code or 0 on success
+ */
+int macvlan_add(struct hostapd_data *hapd, char *if_name, size_t len,
+		const u8 *addr, const char *if_base);
+
+int macvlan_del(struct hostapd_data *hapd, const char *if_name);
+
 #endif /* CONFIG_LIBNL3_ROUTE */
 
 #endif
