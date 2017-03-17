2c695035301860e56e8a53aa76f6ba41ca5e90d1
Michael Braun <michael-dev@fami-braun.de>
2017-03-17 23:26:42 +0100

    FT: add expiration to PMK-R0 and PMK-R1 cache
    IEEE 802.11-2012 says: MSK has a lifetime that limits PMK_R0 and PMK_R1
    lifetime. This is currently stored in r0_key_lifetime, but cache entries
    are not actually removed.
    
    This patch assumes a default MSK lifetime to be 3600s when
    wpa_auth_derive_ptk_ft is called. This matches the default eapol_reauth
    timeout and should probably be passed in from eapol state machine with some
    future changes. For PSK, there is no such lifetime, but it also matters
    less as FT-PSK can be achieved without inter-AP communication.
    
    The expiration timeout is then passed from R0KH to R1KH. The R1KH verifies
    that the given timeout for sanity, it may not exceed the locally configured
    r1_max_key_lifetime.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/wpa_auth.h
+++ src/ap/wpa_auth.h
@@ -74,6 +74,7 @@ struct ft_rrb_frame {
 #define FT_RRB_PMK_R1        10 /* PMK_LEN */
 
 #define FT_RRB_PAIRWISE      11 /* le16 */
+#define FT_RRB_EXPIRES_IN    12 /* le16 seconds */
 
 struct ft_rrb_tlv {
 	le16 type;
@@ -89,6 +90,7 @@ struct ft_rrb_seq {
 
 /* session TLVs:
  *   required: PMK_R1, PMK_R1_NAME, PAIRWISE
+ *   optional: EXPIRES_IN
  *
  * pull frame TLVs:
  *   auth:
@@ -187,6 +189,7 @@ struct wpa_auth_config {
 	int rkh_neg_timeout;
 	int rkh_pull_timeout; /* ms */
 	int rkh_pull_retries;
+	int r1_max_key_lifetime;
 	u32 reassociation_deadline;
 	struct ft_remote_r0kh **r0kh_list;
 	struct ft_remote_r1kh **r1kh_list;
